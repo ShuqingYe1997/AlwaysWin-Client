649374ab5851b3412cc14181a1d16a068245f4ed
Zhihui Xia <xiazhvera@gmail.com>
2021-05-17 00:55:48 -0700

    [WIP] Adding ProductCard.vue and enum

---
+++ src/api/enum.js
@@ -0,0 +1,6 @@
+export const calendarTypeOptions = [
+  { key: 'CN', display_name: 'China' },
+  { key: 'US', display_name: 'USA' },
+  { key: 'JP', display_name: 'Japan' },
+  { key: 'EU', display_name: 'Eurozone' }
+]
--- src/views/dashboard/admin/components/ProductCard.vue
+++ src/views/dashboard/admin/components/ProductCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <router-link :to="'/_id'">
+  <router-link :to="`/${productForm.pid}`">
     <el-card class="box-card-component" style="margin-left:8px;">
       <div slot="header" class="box-card-header">
-        <img src="https://ss7.vzw.com/is/image/VerizonWireless/iphone-12-pro-pacific-blue">
+        <img :src="`${productForm.thumbnail.thumbnail}`">
       </div>
       <div style="position:relative;">
-        <pan-thumb :image="avatar" class="panThumb" />
-        <span class="price">$100.00</span>
+        <pan-thumb :image="sellerInfo.portrait" class="panThumb" />
+        <span class="price"> {{ productForm.productStatus.price }} </span>
         <div style="padding-top:35px;" class="title-item">
-          <span>IPhone XS</span>
+          <span> {{ productForm.title }}</span>
         </div>
         <div class="description-item">
-          <span>Almost new IPhone XS. Original Pack. White. </span>
+          <span>{{ productForm.description }} </span>
         </div>
         <div class="start-item">
           <Countdown deadline="2021/08/13" />
@@ -28,36 +28,83 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import PanThumb from '@/components/PanThumb'
 import Countdown from 'vuejs-countdown'
+import { productDetail } from '@/api/product.js'
+
+const defaultProductForm = {
+  pid: 8,
+  uid: 1,
+  title: '',
+  description: '',
+  cate1: '',
+  cate2: '',
+  cate3: '',
+  createTime: '',
+  startTime: '',
+  endTime: '',
+  startPrice: 0.0,
+  autoWinPrice: 0.0,
+  reservedPrice: 0.0,
+  minIncrement: 1.0,
+  productStatus: {
+    psid: 8,
+    pid: 8,
+    price: 100.0,
+    status: '',
+    endTime: ''
+  },
+  figures: [
+    {
+      fid: 8,
+      pid: 8,
+      url: '',
+      description: '',
+      updatedTime: '',
+      thumbnail: true
+    }
+  ],
+  thumbnail: {
+    fid: 8,
+    pid: 8,
+    url: '',
+    description: '',
+    updatedTime: '',
+    thumbnail: true
+  },
+  canceled: false,
+  passed: true
+}
+
+const defaultSellerInfo =
+{
+  uid: undefined,
+  name: 'TestName',
+  portrait: 'https://ss7.vzw.com/is/image/VerizonWireless/iphone-12-pro-pacific-blue'
+}
 
 export default {
   components: { PanThumb, Countdown },
 
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        success: 'success',
-        pending: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
   data() {
     return {
-      statisticsData: {
-        article_count: 1024,
-        pageviews_count: 1024
-      }
+      productForm: Object.assign({}, defaultProductForm),
+      sellerInfo: Object.assign({}, defaultSellerInfo)
     }
   },
-  computed: {
-    ...mapGetters([
-      'name',
-      'avatar',
-      'roles'
-    ])
+  created() {
+    const id = this.$route.params && this.$route.params.id
+    this.fetchData(id)
+  },
+
+  methods: {
+    fetchData(id) {
+      productDetail(id).then(response => {
+        this.productName = response.data
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   }
 }
 </script>
