34bce507367d2936c44e7ccf1d66c2dd38d8b7aa
Zhihui Xia <xiazhvera@gmail.com>
2021-05-28 00:54:52 -0700

    fix file format

--- src/views/product/components/DetailTable.vue
+++ src/views/product/components/DetailTable.vue
@@ -1,85 +1,96 @@
 <template>
-<div class="chart-wrapper">
-  <div class="container">
-    <table>
-              <thead><th colspan="2">
-          Product Info
-        </th></thead>
-      <tbody>
+  <div class="chart-wrapper">
+    <div class="container">
+      <table>
+        <thead>
+          <th colspan="2">Product Info</th>
+        </thead>
+        <tbody>
           <tr>
             <td style="width: 45%">Catalog</td>
-            <td><el-tag type="info">
-            {{ productCate[productInfo.cate1].text }}
-            </el-tag></td>
+            <td>
+              <el-tag type="info">
+                {{ productCate[productInfo.cate1].text }}
+              </el-tag>
+            </td>
           </tr>
           <tr>
-            <td >Start Price:</td>
-            <td>${{productInfo.startPrice}}</td>
+            <td>Start Price:</td>
+            <td>${{ productInfo.startPrice }}</td>
           </tr>
           <tr>
             <td>Auto Win Price:</td>
-            <td>${{productInfo.autoWinPrice}}</td>
+            <td>${{ productInfo.autoWinPrice }}</td>
           </tr>
           <tr>
             <td>Minimum Increment Price:</td>
-            <td>${{productInfo.minIncrement}}</td>
+            <td>${{ productInfo.minIncrement }}</td>
           </tr>
-          <tr v-if="!readMoreInfo"  >
-            <td colspan="2" style="text-align: center;box-shadow: 0 0 10px rgba(0,0,0,0.1);line-height:12px" >
-              <a @click="activateMoreInfo" class="readmore">
-              READ MORE
-              </a>
+          <tr v-if="!readMoreInfo">
+            <td
+              colspan="2"
+              style="
+                text-align: center;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                line-height: 12px;
+              "
+            >
+              <a class="readmore" @click="activateMoreInfo"> READ MORE </a>
             </td>
           </tr>
           <tr v-if="readMoreInfo">
             <td>Create Time:</td>
-            <td>{{productInfo.createTime | formatDate}}</td>
+            <td>{{ productInfo.createTime | formatDate }}</td>
           </tr>
           <tr v-if="readMoreInfo">
             <td>Start Time:</td>
-            <td>{{productInfo.startTime | formatDate}}</td>
+            <td>{{ productInfo.startTime | formatDate }}</td>
           </tr>
           <tr v-if="readMoreInfo">
             <td>End Time:</td>
-            <td>{{productInfo.endTime | formatDate}}</td>
+            <td>{{ productInfo.endTime | formatDate }}</td>
           </tr>
-
-      </tbody>
-      </table>
-      </div>
-      <table style="line-height:24px">
-        <thead><th>
-          Description
-        </th></thead>
-      <tbody>
-      
-        <tr>
-            <td v-if="!readMoreDesc" style="width:100%">{{productInfo.description.slice(0, 200)}}</td>
-            <td v-if="readMoreDesc" style="width:100%"> {{productInfo.description}}</td>
-       </tr>
-       <tr v-if="!readMoreDesc && productInfo.description.length > 199"  >
-        <td colspan="2" style="text-align: center;box-shadow: 0 0 10px rgba(0,0,0,0.1);line-height:12px" >
-          <a @click="activateMoreDesc" class="readmore">
-          READ MORE
-          </a>
-        </td>
-      </tr>
         </tbody>
       </table>
+    </div>
+    <table style="line-height: 24px">
+      <thead>
+        <th>Description</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td v-if="!readMoreDesc" style="width: 100%">
+            {{ productInfo.description.slice(0, 200) }}
+          </td>
+          <td v-if="readMoreDesc" style="width: 100%">
+            {{ productInfo.description }}
+          </td>
+        </tr>
+        <tr v-if="!readMoreDesc && productInfo.description.length > 199">
+          <td
+            colspan="2"
+            style="
+              text-align: center;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+              line-height: 12px;
+            "
+          >
+            <a class="readmore" @click="activateMoreDesc"> READ MORE </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
-
-
 
 <script>
 import { getProductCate } from '@/api/product'
 
 export default {
-
   props: {
     productInfo: {
       type: Object,
-      default: {} 
+      default: null
     }
   },
   data() {
@@ -89,22 +100,22 @@ export default {
       productCate: getProductCate()
     }
   },
-  methods:{
-    activateMoreDesc(){
-        this.readMoreDesc = true;
+  methods: {
+    activateMoreDesc() {
+      this.readMoreDesc = true
     },
-    activateMoreInfo(){
-        this.readMoreInfo = true;
+    activateMoreInfo() {
+      this.readMoreInfo = true
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .chart-wrapper {
-    padding: 0px 10px 0;
-    margin-bottom: 20px;
-  }
+.chart-wrapper {
+  padding: 0px 10px 0;
+  margin-bottom: 20px;
+}
 
 .container {
   margin-bottom: 32px;
@@ -113,58 +124,51 @@ export default {
 }
 
 table {
-	width: 100%;
-	border-collapse: collapse;
-	overflow: hidden;
-	box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  width: 100%;
+  border-collapse: collapse;
+  overflow: hidden;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   font-size: 14px;
 }
 
 th,
 td {
-	padding: 12px;
-	background-color: rgba(255,255,255,0.2);
-	color: rgb(0, 0, 0);
-}
-
-th {
-	text-align: left;
+  padding: 12px;
+  background-color: rgba(255, 255, 255, 0.2);
   color: rgb(0, 0, 0);
 }
 
+th {
+  text-align: left;
+  color: rgb(0, 0, 0);
+}
 
 tbody {
-	td {
-		position: relative;
-		&:hover {
-			&:before {
-				content: "";
-				position: absolute;
-				left: 0;
-				right: 0;
-				top: -9999px;
-				bottom: -9999px;
-				background-color: rgba(255,255,255,0.2);
-				z-index: -1;
-			}
-		}
-	}
+  td {
+    position: relative;
+    &:hover {
+      &:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: -9999px;
+        bottom: -9999px;
+        background-color: rgba(255, 255, 255, 0.2);
+        z-index: -1;
+      }
+    }
+  }
 }
 
-.des-text{
-  font-size: 16px
+.des-text {
+  font-size: 16px;
 }
 
-.readmoreCol{
-
+.readmoreCol {
 }
 
-
-.readmore{
-  color:#4d5b9b;
+.readmore {
+  color: #4d5b9b;
 }
-
-
-
-
 </style>
